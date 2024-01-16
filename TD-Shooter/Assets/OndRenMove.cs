d99166f6a492d003dace61d79976dfd823990711
OffsetSquire <oscar.bank@elev.ga.lbs.se>
2024-01-16 09:50:54 +0100

    update

--- TD-Shooter/Assets/OndRenMove.cs
+++ TD-Shooter/Assets/OndRenMove.cs
@@ -6,6 +6,7 @@ public class OndRenMove : MonoBehaviour
 {
     private Transform target;
     public float speed;
+    public float minDistance; // Set the minimum distance here
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,12 @@ public class OndRenMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-       
+        float distanceToPlayer = Vector2.Distance(transform.position, target.position);
+
+        if (distanceToPlayer <= minDistance)
+        {
+            // Move towards the player only if within the minimum distance
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
     }
 }
